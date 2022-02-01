25cc30fa6e90ae5e21b5116ec871efd81d0b2d34
bmargerison <bmargerison@outlook.com>
2022-02-01 11:44:16 +0000

    Routing for user class

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Hello world');
+});
+
+/* GET individual user. */
+router.get('/:id', function(req, res, next) {
+  res.send(req.params.id)
+});
+
+/* Create user. */
+router.post('/', function(req, res, next) {
+  
+});
+
+/* Update user. */
+router.patch('/:id', function(req, res, next) {
+  
+});
+
+/* Delete user. */
+router.delete('/:id', function(req, res, next) {
+  
 });
 
 module.exports = router;
