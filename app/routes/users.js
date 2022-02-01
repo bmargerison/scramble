0129c77c5de56888011ed0c83abc5d3669a8f1ea
bmargerison <bmargerison@outlook.com>
2022-02-01 14:21:10 +0000

    Users model and GET and POST endpoints working

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hello world');
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find()
+    res.json()
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  }
 });
 
 /* GET individual user. */
@@ -12,8 +18,20 @@ router.get('/:id', function(req, res, next) {
 });
 
 /* Create user. */
-router.post('/', function(req, res, next) {
-  
+router.post('/', async (req, res) => {
+  console.log(req)
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+
+  try {
+    const newUser = await user.save()
+    res.status(201).json(newUser)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
 });
 
 /* Update user. */
