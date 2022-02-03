582cd2882b2c535fb51edcd523daa3546e8d3f99
bmargerison <bmargerison@outlook.com>
2022-02-03 11:17:43 +0000

    Setup react screen navigation

---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Image, ScrollView, TextInput } from 'react-native';
+
+const Signup = () => {
+  return (
+    <ScrollView>
+
+    </ScrollView>
+  );
+}
+
+export default Signup;
