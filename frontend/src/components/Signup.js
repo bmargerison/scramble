d976caa5f70069c85edff854560425947002b77d
bmargerison <bmargerison@outlook.com>
2022-02-03 11:31:15 +0000

    Simple signup form

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import { View, Text, Image, ScrollView, TextInput } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Button, StyleSheet, Text, Image, ScrollView, TextInput } from 'react-native';
 
 const Signup = () => {
-  return (
-    <ScrollView>
 
-    </ScrollView>
+  return (
+    <View style={styles.container}>
+    <TextInput
+      style={styles.input}
+      placeholder='Username'
+      autoCapitalize="none"
+      placeholderTextColor='white'
+    />
+    <TextInput
+      style={styles.input}
+      placeholder='Email'
+      autoCapitalize="none"
+      placeholderTextColor='white'
+    />
+    <TextInput
+      style={styles.input}
+      placeholder='Password'
+      secureTextEntry={true}
+      autoCapitalize="none"
+      placeholderTextColor='white'
+    />
+    <Button
+      title='Sign Up'
+    />
+  </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    width: 350,
+    height: 55,
+    backgroundColor: '#AFEEEE',
+    margin: 10,
+    padding: 8,
+    color: 'white',
+    borderRadius: 14,
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default Signup;
