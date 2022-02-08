4db6c55a791db38da7bb8a0327af01af1d908f22
bmargerison <bmargerison@outlook.com>
2022-02-08 10:24:22 +0000

    Separate navigation stacks depending on auth status

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import SignupScreen from './src/pages/SignupScreen'
 import LoginScreen from './src/pages/LoginScreen'
 import HomeScreen from './src/pages/HomeScreen'
@@ -8,46 +9,81 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {Provider as AuthProvider} from './src/context/AuthContext.js';
 import {Context as AuthContext} from './src/context/AuthContext';
 
-const Tab = createBottomTabNavigator();
+const AuthStack = createStackNavigator();
 
-export default function App() {
+function authFlow() {
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        <Tab.Navigator
-          initialRouteName="Feed"
-          tabBarOptions={{
-            activeTintColor: '#42f44b',
-          }}>
-          <Tab.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{ 
-                title: "Home",
-                tabBarLabel: "Home"
-              }}
-            />
-          <Tab.Screen
-              name="Signup"
-              component={SignupScreen}
-              options={{ 
-                title: "Sign up here",
-                tabBarLabel: "Signup"
-              }}
-            />
-          <Tab.Screen
-              name="Login"
-              component={LoginScreen}
-              options={{ 
-                title: "Please login",
-                tabBarLabel: "Login"
-              }}
-            />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </AuthProvider>
+    <AuthStack.Navigator>
+      <AuthStack.Screen
+        options={{headerShown: false}}
+        name="Signin"
+        component={LoginScreen}
+      />
+      <AuthStack.Screen
+        options={{headerShown: false}}
+        name="Signup"
+        component={SignupScreen}
+      />
+    </AuthStack.Navigator>
   );
 }
+
+const Tab = createBottomTabNavigator();
+
+function homeFlow() {
+  return (
+    <Tab.Navigator
+    initialRouteName="Feed"
+    tabBarOptions={{
+      activeTintColor: '#42f44b',
+    }}>
+    <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ 
+          title: "Home",
+          tabBarLabel: "Home"
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  const {state} = React.useContext(AuthContext);
+  console.log(state);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {state.token === null ? (
+          <>
+            <Stack.Screen
+              options={{headerShown: false}}
+              name="Auth"
+              component={authFlow}
+            />
+          </>
+        ) : (
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="Home"
+            component={homeFlow}
+          />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default () => {
+  return (
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
