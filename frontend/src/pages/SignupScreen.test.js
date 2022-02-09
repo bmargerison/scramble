87cb8843ca1895e3c167b58d0871195b145a038e
bmargerison <bmargerison@outlook.com>
2022-02-09 15:50:36 +0000

    Revert "Commit before eject"
    This reverts commit 2517e7084cdda437978f4b6581c11976ae5c4ba3.

--- frontend/src/pages/SignupScreen.test.js
+++ frontend/src/pages/SignupScreen.test.js
@@ -1,13 +1,8 @@
-
 import React from 'react';
 import renderer from 'react-test-renderer';
 import SignupScreen from './SignupScreen';
 import {Provider as AuthProvider} from '../context/AuthContext.js';
 import {Context as AuthContext} from '../context/AuthContext';
-import {cleanup, fireEvent, render} from '@testing-library/react';
-
-import ReactDOM from 'react-dom';
-afterEach(cleanup);
 
 test('renders correctly', () => {
   const tree = renderer.create(
@@ -15,15 +10,5 @@ test('renders correctly', () => {
       <SignupScreen />
     </AuthProvider>
   ).toJSON();
-
   expect(tree).toMatchSnapshot();
-});
-
-test('renders individual elements', () => {
-  const {queryByLabelText} = render(
-    <AuthProvider>
-      <SignupScreen />
-    </AuthProvider>
-  )
-  console.log(queryByLabelText('Sign up'))
 });
