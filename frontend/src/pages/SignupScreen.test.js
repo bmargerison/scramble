2517e7084cdda437978f4b6581c11976ae5c4ba3
bmargerison <bmargerison@outlook.com>
2022-02-09 11:12:46 +0000

    Commit before eject

--- frontend/src/pages/SignupScreen.test.js
+++ frontend/src/pages/SignupScreen.test.js
@@ -1,8 +1,13 @@
+
 import React from 'react';
 import renderer from 'react-test-renderer';
 import SignupScreen from './SignupScreen';
 import {Provider as AuthProvider} from '../context/AuthContext.js';
 import {Context as AuthContext} from '../context/AuthContext';
+import {cleanup, fireEvent, render} from '@testing-library/react';
+
+import ReactDOM from 'react-dom';
+afterEach(cleanup);
 
 test('renders correctly', () => {
   const tree = renderer.create(
@@ -10,5 +15,15 @@ test('renders correctly', () => {
       <SignupScreen />
     </AuthProvider>
   ).toJSON();
+
   expect(tree).toMatchSnapshot();
+});
+
+test('renders individual elements', () => {
+  const {queryByLabelText} = render(
+    <AuthProvider>
+      <SignupScreen />
+    </AuthProvider>
+  )
+  console.log(queryByLabelText('Sign up'))
 });
