ab55f619d775669ea3c015ab7cad304ecf682732
bmargerison <bmargerison@outlook.com>
2022-02-09 17:24:37 +0000

    Testing the text inputs and button on Signup screen

--- frontend/src/pages/SignupScreen.test.js
+++ frontend/src/pages/SignupScreen.test.js
@@ -1,14 +1,48 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from 'react';
-import renderer from 'react-test-renderer';
 import SignupScreen from './SignupScreen';
 import {Provider as AuthProvider} from '../context/AuthContext.js';
-import {Context as AuthContext} from '../context/AuthContext';
 
-test('renders correctly', () => {
-  const tree = renderer.create(
-    <AuthProvider>
-      <SignupScreen />
-    </AuthProvider>
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
+import {shallow, mount} from 'enzyme';
+
+
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('signup screen', () => {
+
+  it('should match to snapshot', () => {
+    const component = shallow(    
+      <AuthProvider>
+        <SignupScreen/>
+      </AuthProvider>
+    )
+    expect(component).toMatchSnapshot()
+  });
+
+  it('should have a username, email and password field', () => {
+    const component = mount(    
+      <AuthProvider>
+        <SignupScreen/>
+      </AuthProvider>
+    )
+    expect(component.find('TextInput').first().prop('placeholder')).toEqual('Username')
+    expect(component.find('TextInput').at(2).prop('placeholder')).toEqual('Email')
+    expect(component.find('TextInput').at(4).prop('placeholder')).toEqual('Password')
+  });
+
+  it('should have a signup button', () => {
+    const component = mount(    
+      <AuthProvider>
+        <SignupScreen/>
+      </AuthProvider>
+    )
+    expect(component.find('Button').first().prop('title')).toEqual('Sign Up')
+  });
+
 });
