5f593730948da77394803a933533b4cc22f04980
bmargerison <bmargerison@outlook.com>
2022-02-09 15:46:46 +0000

    Revert "Test rendering of items on Signup screen"
    This reverts commit 80c32b6f4e33ce525b0106c702b009ff36f3deb9.

--- frontend/src/pages/SignupScreen.test.js
+++ frontend/src/pages/SignupScreen.test.js
@@ -1,15 +1,12 @@
-/**
- * @jest-environment jsdom
- */
 
 import React from 'react';
 import renderer from 'react-test-renderer';
 import SignupScreen from './SignupScreen';
 import {Provider as AuthProvider} from '../context/AuthContext.js';
-import {cleanup, render, waitFor} from '@testing-library/react';
-import '@testing-library/jest-dom'
-import '@testing-library/jest-dom/extend-expect'
+import {Context as AuthContext} from '../context/AuthContext';
+import {cleanup, fireEvent, render} from '@testing-library/react';
 
+import ReactDOM from 'react-dom';
 afterEach(cleanup);
 
 test('renders correctly', () => {
@@ -22,22 +19,11 @@ test('renders correctly', () => {
   expect(tree).toMatchSnapshot();
 });
 
-test('renders individual elements', async () => {
-  const { findAllByText, findAllByPlaceholderText } = render(
+test('renders individual elements', () => {
+  const {queryByLabelText} = render(
     <AuthProvider>
       <SignupScreen />
     </AuthProvider>
   )
-
-  const signup = await findAllByText('Sign Up')
-  expect(signup.length).toBe(1)
-
-  const username = await findAllByPlaceholderText('Username')
-  expect(username.length).toBe(1)
-
-  const email = await findAllByPlaceholderText('Email')
-  expect(email.length).toBe(1)
-
-  const password = await findAllByPlaceholderText('Password')
-  expect(password.length).toBe(1)
+  console.log(queryByLabelText('Sign up'))
 });
