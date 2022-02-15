09759c6824e9c70e55dfea939b0f8553c0cf9d88
bmargerison <bmargerison@outlook.com>
2022-02-15 10:35:31 +0000

    Homepage list of lists styling

--- frontend/src/pages/HomeScreen.js
+++ frontend/src/pages/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { SafeAreaView, StyleSheet, Text, Button } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, Button, View, FlatList, TouchableOpacity } from 'react-native';
 import {Context as AuthContext} from '../context/AuthContext';
 
 const HomeScreen = ({ navigation }) => {
@@ -22,14 +22,67 @@ const HomeScreen = ({ navigation }) => {
   }, []);
 
   return (
-    <>
-      {lists.map(list => {
-        return(
-          <Text key={list._id}>{list._user}</Text>
-        )
-      })}
-    </>
+    <View style={styles.container}>
+      <FlatList 
+        style={styles.contentList}
+        columnWrapperStyle={styles.listContainer}
+        data={lists.reverse()}
+        keyExtractor= {(item) => {
+          return item._id;
+        }}
+        renderItem={({item}) => {
+        return (
+          <TouchableOpacity style={styles.card}>
+            <View style={styles.cardContent}>
+              <Text style={styles.name}>{item.date}</Text>
+            </View>
+          </TouchableOpacity>
+        )}}/>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    marginTop:20,
+    backgroundColor:"#ebf0f7"
+  },
+  contentList:{
+    flex:1,
+  },
+  cardContent: {
+    marginLeft:20,
+    marginTop:10
+  },
+
+  card:{
+    shadowColor: '#00000021',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+    elevation: 12,
+
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop:20,
+    backgroundColor:"white",
+    padding: 10,
+    flexDirection:'row',
+    borderRadius:30,
+  },
+
+  name:{
+    fontSize:18,
+    flex:1,
+    alignSelf:'center',
+    color:"#3399ff",
+    fontWeight:'bold'
+  },
+}); 
+
 
 export default HomeScreen;
