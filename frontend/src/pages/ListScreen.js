1475bec96efa5b036fa3adc4edc4de597d1a2cef
bmargerison <bmargerison@outlook.com>
2022-02-17 10:22:11 +0000

    More list page styling

--- frontend/src/pages/ListScreen.js
+++ frontend/src/pages/ListScreen.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, StyleSheet, Text, Button, View, FlatList, TouchableOpacity } from 'react-native';
+import { SafeAreaView, TextInput, Pressable, StyleSheet, Text, Button, View, FlatList, TouchableOpacity, Modal } from 'react-native';
 import {AppStyles} from '../AppStyles';
 
 const ListScreen = ({ route, navigation }) => {
   const [ list, setList ] = useState(route.params);
+  const [ item, setItem ] = useState();
+  const [ modalVisible, setModalVisible ] = useState(false);
 
   useEffect(() => {
 
@@ -16,10 +18,38 @@ const ListScreen = ({ route, navigation }) => {
 
   return (
     <View style={styles.container}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <TextInput
+            style={styles.body}
+            secureTextEntry={true}
+            placeholder="What would you like to add?"
+            onChangeText={setItem}
+            value={item}
+            placeholderTextColor={AppStyles.color.grey}
+            underlineColorAndroid="transparent"
+            />
+            <TouchableOpacity
+              style={styles.addButton}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Add</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
       <Text style={[styles.title, styles.leftTitle]}>{list.item.date}</Text>
-      <TouchableOpacity style={styles.addContainer}>
-          <Text style={styles.buttonText}>Add Item</Text>  
-        </TouchableOpacity>
+      <TouchableOpacity style={styles.addContainer} onPress={() => setModalVisible(true)}>
+        <Text style={styles.buttonText}>Add Item</Text>  
+      </TouchableOpacity>
     </View>
   );
 }
@@ -48,9 +78,62 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 20,
   },
+  addButton: {
+    width: AppStyles.buttonWidth.main,
+    backgroundColor: AppStyles.color.tint,
+    borderRadius: AppStyles.borderRadius.main,
+    padding: 10,
+    marginTop: 20,
+  },
   buttonText: {
     color: AppStyles.color.white,
     alignSelf: "center",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  },
+  body: {
+    height: 42,
+    paddingLeft: 20,
+    paddingRight: 20,
+    color: AppStyles.color.text,
   },
 }); 
 
