889ea19d542bbde66d6ce90dda3485e93ae18bbd
bmargerison <bmargerison@outlook.com>
2022-03-09 11:25:38 +0000

    Update unit tests for adding list id to item

--- app/test/itemModel.test.js
+++ app/test/itemModel.test.js
@@ -6,7 +6,7 @@ process.env.NODE_ENV = 'test'
 server = require('../bin/www');
 const mockingoose = require('mockingoose');
 
-describe("list", function() {
+describe("item", function() {
 
   afterEach(async () => {
     await db.dropDatabase()
@@ -15,6 +15,7 @@ describe("list", function() {
   test("should be invalid if no user", function(done) {
     const item = new Item({
       name: "Diary",
+      _list: "000a000000000000000a0000"
     });
     item.validate(function(err) {
         expect(err.errors._user).to.exist;
@@ -25,6 +26,7 @@ describe("list", function() {
   test("should be invalid if no name", function(done) {
     const item = new Item({
       _user: "000a000000000000000a0000",
+      _list: "000a000000000000000a0000",
     });
     item.validate(function(err) {
         expect(err.errors.name).to.exist;
@@ -32,7 +34,18 @@ describe("list", function() {
     });
   });
 
-  test("item has type, user", async function() {
+  test("should be invalid if no list", function(done) {
+    const item = new Item({
+      name: "Diary",
+      _user: "000a000000000000000a0000",
+    });
+    item.validate(function(err) {
+        expect(err.errors._list).to.exist;
+        done();
+    });
+  });
+
+  test("item has type, user, list", async function() {
     mockingoose.User.toReturn({ 
       _id: "000a000000000000000a0000",
       username: "username",
@@ -42,10 +55,12 @@ describe("list", function() {
     const user = await User.findById({ _id: "000a000000000000000a0000" })
     const item = new Item({
       _user: user._id,
-      type: "dairy"
+      type: "dairy",
+      _list: "000a000000000000000a0000"
     })
     expect(String(item._user)).to.equal(String(user.id))
     expect(String(item.type)).to.equal("dairy")
+    expect(String(item.list)).to.equal("000a000000000000000a0000")
   });
 
 });
