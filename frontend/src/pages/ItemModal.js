af3a5169a6110f38159f6999258243199e16c841
bmargerison <bmargerison@outlook.com>
2022-03-11 11:55:39 +0000

    Refactor item modal

--- frontend/src/pages/ItemModal.js
+++ frontend/src/pages/ItemModal.js
@@ -1,78 +1,16 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { SafeAreaView, 
+import React, { useState } from 'react';
+import {
   TextInput, 
-  Pressable, 
   StyleSheet, 
   Text, 
-  Button, 
   View, 
-  FlatList, 
   TouchableOpacity, 
   Modal } 
 from 'react-native';
 import {AppStyles} from '../AppStyles';
-import axios from "axios";
-import { IP_ADDRESS } from "@env";
-import {Context as AuthContext} from '../context/AuthContext';
-import SelectDropdown from 'react-native-select-dropdown'
 
 const ItemModal = ({show, toggle, setModalItem}) => {
-  // modal forms
   const [ item, setItem ] = useState();
-  const [ type, setType ] = useState();
-  const [ addModalVisible, setAddModalVisible ] = useState(false);
-  const [ typeModalVisible, setTypeModalVisible ] = useState(false);
-
-  // fetch data on each render
-  const [ list, setList ] = useState();
-  const [ types, setTypes ] = useState([]);
-  const [ allItems, setAllItems ] = useState({})
-  const [ userItems, setUserItems ] = useState()
-
-  // state
-  const {state} = useContext(AuthContext);
-
-
-
-  const mapToItem = () => {
-    setAddModalVisible(!addModalVisible)
-    if (userItems.some(saved => saved.name == item)) {
-      addToList(item)
-    } else {
-      setTypeModalVisible(!typeModalVisible)
-    }
-  } 
-
-  const addToList = (item) => {
-    axios
-      .patch(`http://${IP_ADDRESS}:3000/lists/${list._id}`, {
-        item: item,
-      })
-      .then((res) => {
-        setList(res.data)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-
-  const createNewItem = () => {
-    axios
-    .post(`http://${IP_ADDRESS}:3000/items`, {
-      _user: state.userId,
-      _list: list._id,
-      name: item,
-      type: type
-    })
-    .then((res) => {
-      console.log(res)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-    setTypeModalVisible(!typeModalVisible)
-    addToList(item)
-  };
 
   return (
     <View>
@@ -110,38 +48,12 @@ const ItemModal = ({show, toggle, setModalItem}) => {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: AppStyles.fontSize.title,
-    fontWeight: 'bold',
-    color: AppStyles.color.tint,
-    marginTop: 50,
-    marginBottom: 50,
-  },
-  leftTitle: {
-    alignSelf: 'stretch',
-    textAlign: 'left',
-    marginLeft: 20,
-  },
-  addContainer: {
-    width: AppStyles.buttonWidth.main,
-    backgroundColor: AppStyles.color.tint,
-    borderRadius: AppStyles.borderRadius.main,
-    padding: 10,
-    marginBottom: 20,
-  },
   addButton: {
     width: 100,
     backgroundColor: AppStyles.color.tint,
     borderRadius: AppStyles.borderRadius.main,
     padding: 5,
     marginTop: 20,
-  },
-  buttonText: {
-    color: AppStyles.color.white,
-    alignSelf: "center",
   },
   centeredView: {
     flex: 1,
@@ -180,27 +92,6 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     borderWidth: 1,
     backgroundColor: AppStyles.color.white,
-  },
-  dropdownText: {
-    fontSize: 15,
-    backgroundColor: AppStyles.color.white,
-  },
-  items: {
-    fontSize: AppStyles.fontSize.content,
-    color:AppStyles.color.text,
-    marginTop: 2,
-    marginBottom: 2,
-    marginLeft: 20,
-  },
-  heading: {
-    fontSize: AppStyles.fontSize.content,
-    backgroundColor: AppStyles.color.tint,
-    color:AppStyles.color.white,
-    marginTop: 5,
-    marginBottom: 5,
-    marginLeft: 10,
-    marginRight: 10,
-    padding: 2,
   },
 }); 
 
