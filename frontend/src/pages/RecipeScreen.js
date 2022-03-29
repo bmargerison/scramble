1a72b5c70b49605d56245023085bc8151393f70b
bmargerison <bmargerison@outlook.com>
2022-03-29 16:52:32 +0100

    Add back button

--- frontend/src/pages/RecipeScreen.js
+++ frontend/src/pages/RecipeScreen.js
@@ -1,11 +1,19 @@
-import React from "react";
-import { View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from 'react-native';
 import styles from '../styles/styleSheet'
+import Icon from 'react-native-vector-icons/AntDesign';
 
-const RecipeScreen = () => {
+const RecipeScreen = ({ navigation, route }) => {
+  const [ recipe, setItem ] = useState(route.params.recipe);
 
   return (
     <View style={styles.container}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}></Text>
+          <TouchableOpacity style={styles.backButton} onPress={() => navigation.navigate('SearchRecipes')}>
+            <Icon name="leftcircle" size={30} style={styles.backButton} />
+          </TouchableOpacity>
+        </View>
     </View>
   );
 
